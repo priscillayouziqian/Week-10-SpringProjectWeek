bf7e7f46aed0db00eed9d1372972383c1ef4f05e
Zhaoyi Yan <priscillayouziqian@gmail.com>
2022-10-07 23:04:38 -0400

    finished part 2 with Thymeleaf and Spring MVC

--- starter/src/main/java/com/javaunit3/springmvc/MovieController.java
+++ starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -3,7 +3,10 @@ package com.javaunit3.springmvc;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MovieController {
@@ -11,13 +14,35 @@ public class MovieController {
         @Autowired
         private BestMovieService bestMovieService;
 
-        @Autowired
-        private SessionFactory sessionFactory;
+//        @Autowired
+//        private SessionFactory sessionFactory;
 
         @RequestMapping("/")
         public String getIndexPage()
         {
             return "index";
+        }
+
+        @RequestMapping("/bestMovie")
+        public String getBestMoviePage(Model model){ //with a Spring Model parameter
+                model.addAttribute("BestMovie", bestMovieService.getBestMovie().getTitle()); //add an attribute to the model named "BestMovie"
+                //use the bestMovieService object to set it the best movie's title.
+                return "bestMovie"; //the name of corresponding html page we will create.
+        }
+
+        @RequestMapping("/voteForBestMovieForm")
+        public String voteForBestMovieFormPage() {
+                return "voteForBestMovie";
+        }
+
+        @RequestMapping("/voteForBestMovie")
+        public String voteForBestMovie(HttpServletRequest request, Model model) {
+
+                String movieTitle = request.getParameter("movieTitle");
+
+                model.addAttribute("BestMovieVote", movieTitle);
+
+                return "voteForBestMovie";
         }
 
 }
